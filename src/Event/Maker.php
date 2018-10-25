92ffbadbab4111d8aa76f2fc10e0190f5112dd80
wxuns <lzx.iu@foxmail.com>
2018-10-25 14:13:07 +0800

    make controller

--- src/Event/Maker.php
+++ src/Event/Maker.php
@@ -11,19 +11,36 @@ namespace Polite\Console\Event;
 
 class Maker
 {
-    public $type = 'controller';
+    public $type = 'controllers';
     public $filename = '';
+    protected $application = '';
 
     public function __construct($type,$filename)
     {
         $this->type = $type;
         $this->filename = $filename;
-
-        return $this->buildFile();
+        $this->application = realpath(dirname(__FILE__) . '/../../../../../');
     }
 
     public function buildFile()
     {
-        print_r( __DIR__);
+        if ($this->type == 'controllers'){
+            $arr = explode('/',$this->filename);
+            if (!strpos($this->filename,'/')){
+                $controllerPath = $this->application . '/app/controllers/' . $this->filename . '.php';
+            }else{
+                $controllerPath = $this->application . '/app/modules/' . $arr[0] . '/controllers/' . $arr[1] . '.php';
+            }
+            if (!file_exists($controllerPath)){
+                if (!is_dir(dirname($controllerPath))){
+                    mkdir(dirname($controllerPath),0777,true)?:function(){
+                        exit();
+                    };
+                }
+                return file_put_contents($controllerPath,
+                    "<?php\n\nclass " . $arr[count($arr)-1] . "Controller extends Yaf\Controller_Abstract {\n\n}");
+            }
+            return false;
+        }
     }
 }
--- src/Maker/MakeController.php
+++ src/Maker/MakeController.php
@@ -9,7 +9,7 @@
 
 namespace Polite\Console\Maker;
 
-use Console\Event\Maker;
+use Polite\Console\Event\Maker;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -22,21 +22,23 @@ class MakeController extends Command
         $this
             // the name of the command (the part after "bin/console")
             // 命令的名字（"bin/console" 后面的部分）
-            ->setName('make:controller')
+            ->setName('make:controllers')
             // the short description shown while running "php bin/console list"
             // 运行 "php bin/console list" 时的简短描述
-            ->setDescription(' Create a new controller file')
+            ->setDescription(' Create a new controllers file')
 
             // the full command description shown when running the command with
             // the "--help" option
             // 运行命令时使用 "--help" 选项时的完整命令描述
-            ->setHelp("This command allows you to create controller file")
-            ->addArgument('controller', InputArgument::REQUIRED, 'controller name.')
+            ->setHelp("This command allows you to create controllers file")
+            ->addArgument('controllers', InputArgument::REQUIRED, 'controllers name.')
         ;
     }
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $maker = new Maker('controller',$input->getArgument('controller'));
-//        print_r($maker);
+        $maker = new Maker('controllers',$input->getArgument('controllers'));
+        $output->writeln($maker->buildFile()?
+            '<info>controller:' . $input->getArgument('controllers') . ' create success</info>':
+            '<error>controller:' . $input->getArgument('controllers') . ' is already</error>');
     }
 }
