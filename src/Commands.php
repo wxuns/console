d1e5879e25b17ec929cbf21d1c69dac2c8222eaf
wxuns <php7ml@gmail.com>
2018-11-19 22:07:49 +0800

    migrate

--- src/Commands.php
+++ src/Commands.php
@@ -16,14 +16,6 @@ class Commands
             new \Polite\Console\Version(),
             new \Polite\Console\Maker\MakeController(),
             new \Polite\Console\Maker\MakeModel(),
-            new \Doctrine\DBAL\Migrations\Tools\Console\Command\ExecuteCommand(),
-            new \Doctrine\DBAL\Migrations\Tools\Console\Command\GenerateCommand(),
-            new \Doctrine\DBAL\Migrations\Tools\Console\Command\LatestCommand(),
-            new \Doctrine\DBAL\Migrations\Tools\Console\Command\MigrateCommand(),
-            new \Doctrine\DBAL\Migrations\Tools\Console\Command\DiffCommand(),
-            new \Doctrine\DBAL\Migrations\Tools\Console\Command\UpToDateCommand(),
-            new \Doctrine\DBAL\Migrations\Tools\Console\Command\StatusCommand(),
-            new \Doctrine\DBAL\Migrations\Tools\Console\Command\VersionCommand()
         ];
         return $commit;
     }
