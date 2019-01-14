99525eed08f7e8d345234e83e9cf96371984f904
lizhixu <php7ml@gmail.com>
2019-01-14 03:02:22 +0000

    Apply fixes from StyleCI

--- src/Console.php
+++ src/Console.php
@@ -4,7 +4,7 @@
  * Author: wxuns
  * Link: https://www.wxuns.cn
  * Date: 2019/1/11
- * Time: 15:36
+ * Time: 15:36.
  */
 
 namespace Polite\Console;
@@ -16,8 +16,8 @@ class Console
     public static function AddCommands($command = [])
     {
         $application = new Application();
-        foreach ($command as $comm){
-            $application->add(new $comm);
+        foreach ($command as $comm) {
+            $application->add(new $comm());
         }
         $application->run();
     }
